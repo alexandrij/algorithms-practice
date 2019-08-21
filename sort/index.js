e25f4ef037b99daf618aa8c0f91116000a73a9a8
adimitriev <adimitriev@niisokb.ru>
2019-08-21 09:59:00 +0300

    quick sort

--- sort/index.js
+++ sort/index.js
@@ -36,19 +36,76 @@ const selectionSort = (arr) => {
     console.log('selectionSort:', counter);
     return arr;
 };
-
 /**
- * n log n <=> 2^x = n
- * ln x = 10^y = 6
- * 
- * quickSort([3, 5, 1, 8, 3]):
- * 1) pivot = 1; left = []; right = [3, 5, 8, 3]; res = [][1][3, 5, 8, 3]
- * 2) pivot = 8; left = [3, 5, 3]; right = [];    res = [][1][3, 5, 3][8][]
- * 3) pivot = 5; left = [3, 3]; right = [];       res = [][1][3, 3][5][][8][]
- * 3) pivot = 3; left = [3]; right = [];          res = [][1][3][3][][5][][8][]
+ * quickSort([3, 5, 4, 8, 3]):
+ * start:0, end:4, pivot:4, left:3, right:1, arr:[3,3,4,8,5]
+ * start:0, end:1, pivot:3, left:1, right:0, arr:[3,3,4,8,5]
+ * start:3, end:4, pivot:8, left:4, right:3, arr:[3,3,4,5,8]
  */
 const quickSort = (arr) => {
-    var counter = 0;
+    let counter = 0;
+
+    const sorter = (arr, start, end) => {
+        if (arr.length < 2) { return arr; }
+
+        start = typeof start === 'number' ? start : 0;
+        end = typeof end === 'number' ? end : arr.length - 1;
+
+        if ((end - start) < 1) { return arr; }
+
+        const pivot = arr[Math.floor((start + end) / 2)];
+        let left = start;
+        let right = end;
+        let temp;
+
+        while (left <= right) {
+            while (arr[left] < pivot) {
+                left++;
+                counter++;
+            }
+            while (arr[right] > pivot) {
+                right--;
+                counter++;
+            }
+            if (arr[left] >= pivot && arr[right] <= pivot) {
+                counter++;
+            }
+            if (left <= right) {
+                temp = arr[left];
+                arr[left] = arr[right];
+                arr[right] = temp;
+                left++;
+                right--;
+            }
+        }
+        if (right > start) {
+            sorter(arr, start, right);
+        }
+        if (left < end) {
+            sorter(arr, left, end);
+        }
+        return arr;
+    }
+
+    const res = sorter(arr);
+    console.log('quickSort', counter);
+    return res;
+};
+
+
+/**
+ * 
+ * quickSort2([3, 5, 1, 8, 3]):
+ * 
+ * pivot":1,"arr":[3,5,1,8,3] => [1][3,5,8,3]
+ * pivot":8,"arr":[3,5,8,3] => [1][3,5,3][8]
+ * pivot":5,"arr":[3,5,3] => [1][3,3][5][8]
+ * pivot":3,"arr":[3,3] => [1][3][3][5][8]
+ * 
+ * counter
+ */
+const quickSort2 = (arr) => {
+    let counter = 0;
     const sorter = (arr) => {
         if (arr.length < 2) { return arr; }
 
@@ -65,6 +122,10 @@ const quickSort = (arr) => {
             }
             counter++;
         }
+        console.log(JSON.stringify({
+            pivot: arr[pivot],
+            arr: arr
+        }));
         return sorter(a).concat(arr[pivot], sorter(b));
     };
     const res = sorter(arr);
