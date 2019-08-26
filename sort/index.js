ac3dce6d928f27f8bff282b4d6fca69f03f9aa74
adimitriev <adimitriev@niisokb.ru>
2019-08-26 17:02:48 +0300

    fabinacci + binarySearch algoritms

--- sort/index.js
+++ sort/index.js
@@ -132,3 +132,26 @@ const quickSort2 = (arr) => {
     console.log('quickSort', counter);
     return res;
 };
+
+const mergeSort = (arr) => {
+    if (arr.length < 2) { return arr; }
+
+    let pivot = arr[Math.floor(arr.length / 2)];
+    let left = [];
+    let right = [];
+
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] < pivot) {
+            left.push(arr[i]);
+        }
+        if (arr[i] > pivot) {
+            right.push(arr[i]);
+        }
+    }
+
+    return [].concat(
+        mergeSort(left),
+        pivot,
+        mergeSort(right)
+    );
+}
