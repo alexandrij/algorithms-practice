b96042c09fb5a778bf70c4fab7eeb6bce8240bcb
adimitriev <adimitriev@niisokb.ru>
2020-11-30 21:20:01 +0300

    Pool запросов

---
+++ async/request-pool.js
@@ -0,0 +1,28 @@
+const fetch = require('node-fetch');
+
+const requestPool = async (urls, callback, limit = 5) => {
+    if (urls.length === 0)
+        return;
+
+    while (urls.length > 0) {
+        callback(await Promise.all(urls.splice(0, limit).map(url => fetch(url))));
+    }
+};
+
+const urls = [
+    "http://example-app/async/data/1.json",
+    "http://example-app/async/data/2.json",
+    "http://example-app/async/data/3.json",
+    "http://example-app/async/data/4.json",
+    "http://example-app/async/data/5.json",
+    "http://example-app/async/data/6.json",
+    "http://example-app/async/data/7.json",
+    "http://example-app/async/data/8.json",
+    "http://example-app/async/data/9.json",
+    "http://example-app/async/data/10.json",
+];
+
+requestPool([...urls, ...urls], (results) => {
+    console.log('----');
+    console.log(...results);
+}, 3)
