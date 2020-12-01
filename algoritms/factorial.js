78b55810e2cb7de46b898e425f5a1af1fe7ae5a5
adimitriev <adimitriev@niisokb.ru>
2020-12-01 10:14:16 +0300

    Тесты вычисления факториала

--- algoritms/factorial.js
+++ algoritms/factorial.js
@@ -1,12 +1,10 @@
-const factorial = (n) => {
+module.exports = factorial = (n) => {
+    if (!Number.isInteger(n)) {
+        throw new Error(`Value "${n}" is not integer`);
+    }
     let res = 1;
     while (n > 0) {
         res *= n--;
     }
     return res;
 }
-
-console.log(factorial(-1));
-console.log(factorial(0));
-console.log(factorial(3));
-console.log(factorial(5));
