74fa18e56e980db0f0ce34e8d1cea82e398137e3
adimitriev <Aleksandr.Dimitriev@kronshtadt.ru>
2021-09-27 09:40:08 +0300

    Улучшение алгоритма поиска одинаковых элементов массиве

--- src/yandex/find-equal-elements.js
+++ src/yandex/find-equal-elements.js
@@ -1,20 +1,50 @@
 // Есть два сортированных массива с числами.
 // Нужно написать функцию, которая возвращает новый массив,
 // содержащий элементы, которые встречаются в обоих массивах.
-const findEqualElements = (arr1, arr2) => {
-    const mapArr2 = arr2.reduce((map, el) => (map.has(el) ? map.set(el, map.get(el) + 1) : map.set(el, 1)), new Map());
+// const findEqualElements = (arr1, arr2) => {
+//     let count = 0;
+//     const mapArr2 = arr2.reduce((map, el) => {
+//         count++;
+//         return (map.has(el) ? map.set(el, map.get(el) + 1) : map.set(el, 1));
+//     }, new Map());
+//
+//     const res = arr1.filter(el => {
+//         count++;
+//         if (mapArr2.has(el) && mapArr2.get(el) > 0) {
+//             mapArr2.set(el, mapArr2.get(el) - 1);
+//             return true;
+//         }
+//         return false;
+//     });
+//     console.log(count);
+//     return res;
+// };
 
-    return arr1.filter(el => {
-        if (mapArr2.has(el) && mapArr2.get(el) > 0) {
-            mapArr2.set(el, mapArr2.get(el) - 1);
-            return true;
+const findEqualElements = (arr1, arr2) => {
+    const res = [];
+    let i1 = 0;
+    let i2 = 0;
+    while (i1 < arr1.length && i2 < arr2.length) {
+        while (arr1[i1] < arr2[i2]) {
+            i1++;
         }
-        return false;
-    });
-}
+        while (arr1[i1] > arr2[i2]) {
+            i2++;
+        }
+        if (arr1[i1] === arr2[i2]) {
+            res.push(arr1[i1]);
+        }
+        i1++;
+        i2++;
+    }
+    return res;
+};
+
 module.exports = { findEqualElements: findEqualElements };
 
 // Примеры
 console.log(findEqualElements([1, 2, 3], [2])); // => [2]
+console.log(findEqualElements([0, 1, 2, 3], [0, 2])); // => [2]
 console.log(findEqualElements([2], [1, 2, 3])); // => [2]
 console.log(findEqualElements([1, 2, 2, 3], [2, 2, 2, 2, 4, 4, 4, 4])); // => [2, 2]
+console.log(findEqualElements([1, 2, 2, 3, 4, 4, 5, 6, 6], [2, 2, 2, 2, 4, 4, 4, 4])); // => [2, 2]
