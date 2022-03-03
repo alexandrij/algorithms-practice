bdfafa603852e0d12dee5632b0a1fb3fb9fe3892
adimitriev <a.dimitriev.86@yandex.ru>
2022-03-03 09:09:10 +0300

    Исправление комментарий

--- src/async/try-request.js
+++ src/async/try-request.js
@@ -7,3 +7,26 @@ const tryRequest = (request, n) => {
 tryRequest(fetch('http://test.ru'), 3)
     .then(r => console.log(r))
     .catch(e => console.log(e));
+
+//
+// /**
+//  * Реализовать асинхронную функцию, которая выполняется не более n - раз, пока не получит успешный ответ.
+//  * @param n - максимальное кол-во попыток
+//  */
+// const tryAuth = (n) => {
+//
+// }
+//
+// const auth = (() => {
+//     let cur = 0;
+//     return (callback) => {
+//         setTimeout(() => {
+//             if (cur++ === 3) {
+//                 cur = 0;
+//                 callback(null, { sessionId: 'sssss' });
+//             } else {
+//                 callback(new Error('auth is failed'));
+//             }
+//         }, 400);
+//     }
+// })();
