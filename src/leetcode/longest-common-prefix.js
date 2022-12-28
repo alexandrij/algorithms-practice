82c3e2ed1f02d0281577c97a261a9464aa0123c1
adimitriev <a.dimitriev.86@yandex.ru>
2022-12-28 09:59:38 +0300

    leetcode: longest common prefix

---
+++ src/leetcode/longest-common-prefix.js
@@ -0,0 +1,19 @@
+const longestCommonPrefix = (strs) => {
+  let longestPfx = strs[0];
+  for (let i = 1, len = strs.length; i < len; i++) {
+    for (let j = 0, l = longestPfx.length; j < l; j++) {
+      if (longestPfx[j] !== strs[i][j]) {
+        longestPfx = longestPfx.substring(0, j);
+        break;
+      }
+    }
+  }
+  return longestPfx;
+}
+
+console.log(longestCommonPrefix(['flower', 'flow', 'flight'])); // fl
+console.log(longestCommonPrefix(['dog', 'racecar', 'car'])); // ''
+console.log(longestCommonPrefix([''])); // ''
+console.log(longestCommonPrefix(['dddd'])); // ''
+console.log(longestCommonPrefix(['dddd', 'ssdd'])); // ''
+console.log(longestCommonPrefix(['a', 'as'])); // ''
