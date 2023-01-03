1485dbddec946b3b1a0240b6b0f2e9ce39ebb70c
adimitriev <a.dimitriev.86@yandex.ru>
2023-01-03 10:58:52 +0300

    max length numbers

---
+++ src/yandex/max-len-numbers.js
@@ -0,0 +1,19 @@
+function maxLenNumbers(numbers) {
+  let maxLen = 0
+  let len = 0;
+  numbers.forEach((n) => {
+    if (n > 0) {
+      len += 1;
+      maxLen = Math.max(len, maxLen);
+    } else if (maxLen > 0) {
+      len = 0;
+    }
+  });
+  return maxLen;
+}
+
+console.log(maxLenNumbers([0, 0, 1, 1, 1, 0, 0, 1, 1])) // 3
+console.log(maxLenNumbers([0, 0, 1, 1, 0, 0, 1, 1])) // 2
+console.log(maxLenNumbers([1, 1, 1, 1])) // 4
+console.log(maxLenNumbers([0, 0, 0, 0])) // 0
+console.log(maxLenNumbers([])) // 0
