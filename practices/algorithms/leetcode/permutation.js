1e5e969508e34ff79eb108e08b68ce258f0ae950
aadimitriev <a.dimitriev.86@yandex.ru>
2024-05-29 14:17:31 +0300

    permutation algorithm

--- practices/algorithms/leetcode/permutation.js
+++ practices/algorithms/leetcode/permutation.js
@@ -1,76 +1,3 @@
-// [a, b]  => [a,b], [b,a]
-// [a,b,c] => [a,b,c], [b,a,c], [b,c,a], [c,b,a], [c,a,b], [a,c,b]
-
-/**
- * 3 * 2 * 1 | 0, 1 <-> 2
- *
- * // abc: ab c: abc, cab; ba c: bac, cba; bc a: bca, abc
- *
- * abc:
- * ab ba c
- *
- * abc
- * bac
- * cab
- * cba
- *
- * abc <-> acb: 1 <-> 2
- * ac ca b
- *
- * acb
- * cab +
- * bac +
- * bca
- *
- */
-
-import { factorial } from './factorial.js';
-import { nextPermutation } from './next-permutation.js';
-
-/**
- * 4 * 3 * 2 * 1 | 0, 1 <-> 2, 2 <->3
- *
- * abcd:
- * ab ba cd dc
- *
- * abcd
- * abdc
- * bacd
- * badc
- *
- * cdab
- * cdba
- * dcab
- * dcba
- *
- * abcd <-> acbd: b <-> c: 1 <-> 2
- * ac ca bd db
- *
- * acbd
- * acdb
- * cabd
- * cadb
- *
- * bdac
- * bdca
- * dbac
- * dbca
- *
- * abcd <-> adcb: b <-> d: 2 <-> 3
- * ad da cb bc
- *
- * adcb
- * adbc
- * dacb
- * dabc
- *
- * cbad
- * cbda
- * bcad
- * bcda
- *
- */
-
 function swap(str, a, b) {
   const arr = str.split('');
   const temp = arr[a];
@@ -88,28 +15,10 @@ export const permutation = (str, start = 0) => {
 
   for (let i = start + 1; i < str.length; i++) {
     const permutated = swap(str, start, i);
+
     result.push(permutated);
     result.push(...permutation(permutated, start + 1));
   }
 
   return result;
 };
-
-{
-  const str = 'abc';
-  const res = permutation(str);
-  const exLength = factorial(str.length) - 1;
-  console.log(exLength, res.length, new Set(res).size, res);
-}
-{
-  const str = 'abcd';
-  const res = permutation(str);
-  const exLength = factorial(str.length) - 1;
-  console.log(exLength, res.length, new Set(res).size, res);
-}
-{
-  const str = 'abcdef';
-  const res = permutation(str);
-  const exLength = factorial(str.length) - 1;
-  console.log(exLength, res.length, new Set(res).size, res);
-}
--- practices/algorithms/leetcode/permutation.spec.js
+++ practices/algorithms/leetcode/permutation.spec.js
@@ -8,6 +8,7 @@ describe('algorithms: perumation', () => {
       const permutations = permutation(example);
       const uniquePermutations = new Set(permutations);
       expect(uniquePermutations.size).toBe(factorial(example.length) - 1);
+      expect(Array.from(uniquePermutations)).toEqual(permutations.sort((a, b) => a - b));
       expect(permutations[0]).toBe('acb');
       expect(permutations[1]).toBe('bac');
       expect(permutations[permutations.length - 1]).toBe('cab');
@@ -20,6 +21,7 @@ describe('algorithms: perumation', () => {
       const permutations = permutation(example);
       const uniquePermutations = new Set(permutations);
       expect(uniquePermutations.size).toBe(factorial(example.length) - 1);
+      expect(Array.from(uniquePermutations)).toEqual(permutations.sort((a, b) => a - b));
       expect(permutations[0]).toBe('abcdfe');
       expect(permutations[1]).toBe('abcedf');
     });
