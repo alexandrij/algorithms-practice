8fad915a4c132653e8c30102b88ed1583fcaee5e
aadimitriev <a.dimitriev.86@yandex.ru>
2024-08-07 10:19:33 +0300

    benchmark: assign property to object

---
+++ practices/benchmark/benchmark-object-assign.js
@@ -0,0 +1,299 @@
+const streams = {
+  '/prprprpr/visisisisi/text_view_facing': {
+    stream: '/prprprpr/visisisisi',
+    substream: '/text_view_facing',
+    timestamp: 'Tue Mar 05 2024 09:05:48 GMT+0300 (Moscow Standard Time)',
+  },
+  '/visisisisis/taget-path-vel/path': {
+    stream: '/visisisisis/taget-path-vel',
+    substream: '/path',
+    timestamp: 'Tue Mar 05 2024 09:05:48 GMT+0300 (Moscow Standard Time)',
+  },
+  '/visisisisis/tvtraaa/path': {
+    stream: '/visisisisis/tvtraaa',
+    substream: '/path',
+    timestamp: 'Tue Mar 05 2024 09:05:48 GMT+0300 (Moscow Standard Time)',
+  },
+  '/visisisisis/wheel_direction/path': {
+    stream: '/visisisisis/wheel_direction',
+    substream: '/path',
+    timestamp: 'Tue Mar 05 2024 09:05:48 GMT+0300 (Moscow Standard Time)',
+  },
+  '/perception/cloud_obstacles/visualization/cloud_clusters_visualization/path': {
+    stream: '/perception/cloud_obstacles/visualization/cloud_clusters_visualization',
+    substream: '/path',
+    timestamp: 'Tue Mar 05 2024 09:05:48 GMT+0300 (Moscow Standard Time)',
+  },
+  '/planning/visualization/maneuvers/path': {
+    stream: '/planning/visualization/maneuvers',
+    substream: '/path',
+    timestamp: 'Tue Mar 05 2024 09:05:48 GMT+0300 (Moscow Standard Time)',
+  },
+  '/prprprpr/visisisisi/distance_circles/path': {
+    stream: '/prprprpr/visisisisi/distance_circles',
+    substream: '/path',
+    timestamp: 'Tue Mar 05 2024 09:05:48 GMT+0300 (Moscow Standard Time)',
+  },
+  '/prprprpr/visisisisi/path': {
+    stream: '/prprprpr/visisisisi',
+    substream: '/path',
+    timestamp: 'Tue Mar 05 2024 09:05:48 GMT+0300 (Moscow Standard Time)',
+  },
+  '/planning/visualization/turn_signals/mesh': {
+    stream: '/planning/visualization/turn_signals',
+    substream: '/mesh',
+    timestamp: 'Tue Mar 05 2024 09:05:48 GMT+0300 (Moscow Standard Time)',
+  },
+  '/prprprpr/visisisisi/mesh': {
+    stream: '/prprprpr/visisisisi',
+    substream: '/mesh',
+    timestamp: 'Tue Mar 05 2024 09:05:48 GMT+0300 (Moscow Standard Time)',
+  },
+  '/prprprpr/visisisisi/following/path': {
+    stream: '/prprprpr/visisisisi/following',
+    substream: '/path',
+    timestamp: 'Tue Mar 05 2024 09:05:48 GMT+0300 (Moscow Standard Time)',
+  },
+  '/planning/visualization/stop_line/path': {
+    stream: '/planning/visualization/stop_line',
+    substream: '/path',
+    timestamp: 'Tue Mar 05 2024 09:05:48 GMT+0300 (Moscow Standard Time)',
+  },
+  '/visisisisis/tracked_3d_objects/B/text_view_facing': {
+    stream: '/visisisisis/tracked_3d_objects/B',
+    substream: '/text_view_facing',
+    timestamp: 'Tue Mar 05 2024 09:05:48 GMT+0300 (Moscow Standard Time)',
+  },
+  '/visisisisis/tracked_3d_objects/Bc/text_view_facing': {
+    stream: '/visisisisis/tracked_3d_objects/Bc',
+    substream: '/text_view_facing',
+    timestamp: 'Tue Mar 05 2024 09:05:48 GMT+0300 (Moscow Standard Time)',
+  },
+  '/visisisisis/tracked_3d_objects/P/text_view_facing': {
+    stream: '/visisisisis/tracked_3d_objects/P',
+    substream: '/text_view_facing',
+    timestamp: 'Tue Mar 05 2024 09:05:48 GMT+0300 (Moscow Standard Time)',
+  },
+  '/visisisisis/tracked_3d_objects/V/text_view_facing': {
+    stream: '/visisisisis/tracked_3d_objects/V',
+    substream: '/text_view_facing',
+    timestamp: 'Tue Mar 05 2024 09:05:48 GMT+0300 (Moscow Standard Time)',
+  },
+  '/visisisisis/tracked_3d_objects/Tr/text_view_facing': {
+    stream: '/visisisisis/tracked_3d_objects/Tr',
+    substream: '/text_view_facing',
+    timestamp: 'Tue Mar 05 2024 09:05:48 GMT+0300 (Moscow Standard Time)',
+  },
+  '/chassis/power_train/state': {
+    stream: '/chassis/power_train',
+    substream: '/state',
+    timestamp: 'Tue Mar 05 2024 09:05:48 GMT+0300 (Moscow Standard Time)',
+  },
+  '/diagnostics/localization/localization': {
+    stream: '/diagnostics/localization',
+    substream: '/localization',
+    timestamp: 'Tue Mar 05 2024 09:05:48 GMT+0300 (Moscow Standard Time)',
+  },
+  '/diagnostics/control/input_monitor': {
+    stream: '/diagnostics/control',
+    substream: '/input_monitor',
+    timestamp: 'Tue Mar 05 2024 09:05:48 GMT+0300 (Moscow Standard Time)',
+  },
+  '/visisisisis/tracked_3d_objects/Bc/mesh': {
+    stream: '/visisisisis/tracked_3d_objects/Bc',
+    substream: '/mesh',
+    timestamp: 'Tue Mar 05 2024 09:05:48 GMT+0300 (Moscow Standard Time)',
+  },
+  '/visisisisis/tracked_3d_objects/V/mesh': {
+    stream: '/visisisisis/tracked_3d_objects/V',
+    substream: '/mesh',
+    timestamp: 'Tue Mar 05 2024 09:05:48 GMT+0300 (Moscow Standard Time)',
+  },
+  '/visisisisis/tracked_3d_objects/Tr/mesh': {
+    stream: '/visisisisis/tracked_3d_objects/Tr',
+    substream: '/mesh',
+    timestamp: 'Tue Mar 05 2024 09:05:48 GMT+0300 (Moscow Standard Time)',
+  },
+  '/visisisisis/tracked_3d_objects/P/mesh': {
+    stream: '/visisisisis/tracked_3d_objects/P',
+    substream: '/mesh',
+    timestamp: 'Tue Mar 05 2024 09:05:48 GMT+0300 (Moscow Standard Time)',
+  },
+  '/visisisisis/tracked_3d_objects/Cn/mesh': {
+    stream: '/visisisisis/tracked_3d_objects/Cn',
+    substream: '/mesh',
+    timestamp: 'Tue Mar 05 2024 09:05:48 GMT+0300 (Moscow Standard Time)',
+  },
+  '/visisisisis/tracked_3d_objects/B/mesh': {
+    stream: '/visisisisis/tracked_3d_objects/B',
+    substream: '/mesh',
+    timestamp: 'Tue Mar 05 2024 09:05:48 GMT+0300 (Moscow Standard Time)',
+  },
+  '/pose/covariance': {
+    stream: '/pose',
+    substream: '/covariance',
+    timestamp: 'Tue Mar 05 2024 09:05:48 GMT+0300 (Moscow Standard Time)',
+  },
+  '/pose/trail': {
+    stream: '/pose',
+    substream: '/trail',
+    timestamp: 'Tue Mar 05 2024 09:05:48 GMT+0300 (Moscow Standard Time)',
+  },
+  '/visisisisis/tracked_3d_objects/Cn/text_view_facing': {
+    stream: '/visisisisis/tracked_3d_objects/Cn',
+    substream: '/text_view_facing',
+    timestamp: 'Tue Mar 05 2024 09:05:48 GMT+0300 (Moscow Standard Time)',
+  },
+  '/hdmap/visualization/traffic_lights_lens/mesh': {
+    stream: '/hdmap/visualization/traffic_lights_lens',
+    substream: '/mesh',
+    timestamp: 'Tue Mar 05 2024 09:05:48 GMT+0300 (Moscow Standard Time)',
+  },
+  '/diagnostics/odometry_velocity': {
+    stream: '/diagnostics',
+    substream: '/odometry_velocity',
+    timestamp: 'Tue Mar 05 2024 09:05:48 GMT+0300 (Moscow Standard Time)',
+  },
+  '/diagnostics/cloud_builder': {
+    stream: '/diagnostics',
+    substream: '/cloud_builder',
+    timestamp: 'Tue Mar 05 2024 09:05:48 GMT+0300 (Moscow Standard Time)',
+  },
+  '/diagnostics/cpu': {
+    stream: '/diagnostics',
+    substream: '/cpu',
+    timestamp: 'Tue Mar 05 2024 09:05:48 GMT+0300 (Moscow Standard Time)',
+  },
+  '/diagnostics/chassis': {
+    stream: '/diagnostics',
+    substream: '/chassis',
+    timestamp: 'Tue Mar 05 2024 09:05:48 GMT+0300 (Moscow Standard Time)',
+  },
+  '/chassis/control_path/state': {
+    stream: '/chassis/control_path',
+    substream: '/state',
+    timestamp: 'Tue Mar 05 2024 09:05:48 GMT+0300 (Moscow Standard Time)',
+  },
+  '/hdmap/visualization/crossroads/path': {
+    stream: '/hdmap/visualization/crossroads',
+    substream: '/path',
+    timestamp: 'Tue Mar 05 2024 09:05:48 GMT+0300 (Moscow Standard Time)',
+  },
+  '/hdmap/visualization/ways/dashed_to_solid/path': {
+    stream: '/hdmap/visualization/ways/dashed_to_solid',
+    substream: '/path',
+    timestamp: 'Tue Mar 05 2024 09:05:48 GMT+0300 (Moscow Standard Time)',
+  },
+  '/hdmap/visualization/ways/solid/path': {
+    stream: '/hdmap/visualization/ways/solid',
+    substream: '/path',
+    timestamp: 'Tue Mar 05 2024 09:05:48 GMT+0300 (Moscow Standard Time)',
+  },
+  '/hdmap/visualization/ways/virtual/path': {
+    stream: '/hdmap/visualization/ways/virtual',
+    substream: '/path',
+    timestamp: 'Tue Mar 05 2024 09:05:48 GMT+0300 (Moscow Standard Time)',
+  },
+  '/hdmap/visualization/ways/solid_dashed/path': {
+    stream: '/hdmap/visualization/ways/solid_dashed',
+    substream: '/path',
+    timestamp: 'Tue Mar 05 2024 09:05:48 GMT+0300 (Moscow Standard Time)',
+  },
+  '/hdmap/visualization/ways/double_solid/path': {
+    stream: '/hdmap/visualization/ways/double_solid',
+    substream: '/path',
+    timestamp: 'Tue Mar 05 2024 09:05:48 GMT+0300 (Moscow Standard Time)',
+  },
+  '/hdmap/visualization/ways/dashed_cross/path': {
+    stream: '/hdmap/visualization/ways/dashed_cross',
+    substream: '/path',
+    timestamp: 'Tue Mar 05 2024 09:05:48 GMT+0300 (Moscow Standard Time)',
+  },
+  '/hdmap/visualization/crosswalks/path': {
+    stream: '/hdmap/visualization/crosswalks',
+    substream: '/path',
+    timestamp: 'Tue Mar 05 2024 09:05:48 GMT+0300 (Moscow Standard Time)',
+  },
+  '/hdmap/visualization/traffic_lights/mesh': {
+    stream: '/hdmap/visualization/traffic_lights',
+    substream: '/mesh',
+    timestamp: 'Tue Mar 05 2024 09:05:48 GMT+0300 (Moscow Standard Time)',
+  },
+  '/hdmap/visualization/ways/border/path': {
+    stream: '/hdmap/visualization/ways/border',
+    substream: '/path',
+    timestamp: 'Tue Mar 05 2024 09:05:48 GMT+0300 (Moscow Standard Time)',
+  },
+  '/hdmap/visualization/ways/crosswalk_part/path': {
+    stream: '/hdmap/visualization/ways/crosswalk_part',
+    substream: '/path',
+    timestamp: 'Tue Mar 05 2024 09:05:48 GMT+0300 (Moscow Standard Time)',
+  },
+  '/hdmap/visualization/ways/dashed/path': {
+    stream: '/hdmap/visualization/ways/dashed',
+    substream: '/path',
+    timestamp: 'Tue Mar 05 2024 09:05:48 GMT+0300 (Moscow Standard Time)',
+  },
+  '/hdmap/visualization/stop_lines/path': {
+    stream: '/hdmap/visualization/stop_lines',
+    substream: '/path',
+    timestamp: 'Tue Mar 05 2024 09:05:48 GMT+0300 (Moscow Standard Time)',
+  },
+  '/map_topic': {
+    stream: '/map_topic',
+    substream: '',
+    timestamp: 'Tue Mar 05 2024 09:05:48 GMT+0300 (Moscow Standard Time)',
+  },
+  '/diagnostics/ram': {
+    stream: '/diagnostics',
+    substream: '/ram',
+    timestamp: 'Tue Mar 05 2024 09:05:48 GMT+0300 (Moscow Standard Time)',
+  },
+  '/diagnostics/planning': {
+    stream: '/diagnostics',
+    substream: '/planning',
+    timestamp: 'Tue Mar 05 2024 09:05:48 GMT+0300 (Moscow Standard Time)',
+  },
+};
+
+function objectAssign() {
+  const m1 = process.memoryUsage();
+  const t1 = performance.now();
+  const newStreams = {};
+  for (const key in streams) {
+    Object.assign(newStreams, { [key]: streams[key] });
+  }
+  return {
+    time: performance.now() - t1,
+    memory: process.memoryUsage().heapUsed - m1.heapUsed,
+  };
+}
+
+function objectSpread() {
+  const m1 = process.memoryUsage();
+  const t1 = performance.now();
+  let newStreams = {};
+  for (const key in streams) {
+    newStreams = {
+      ...newStreams,
+      [key]: streams[key],
+    };
+    // console.info('heap increase:', process.memoryUsage().heapUsed - m1.heapUsed);
+  }
+  return {
+    time: performance.now() - t1,
+    memory: process.memoryUsage().heapUsed - m1.heapUsed,
+  };
+}
+
+console.group(`benchmark add attribute to object with assign vs spread`);
+for (const len of [1]) {
+  const { time: assignSpeed, memory: assignMemory } = objectAssign();
+  const { time: spreadSpeed, memory: spreadMemory } = objectSpread();
+  console.group(`*** benchmark ${len} ***`);
+  console.info(`Object.assign: time: ${assignSpeed.toFixed(4)}мс.; memory: ${assignMemory}`);
+  console.info(`Spread: time: ${spreadSpeed.toFixed(4)}мс.; memory: ${spreadMemory}`);
+  console.info(`Object.assign vs Spread: ${(spreadSpeed / assignSpeed).toFixed(0)}`);
+  console.groupEnd();
+  console.info('\n');
+}
